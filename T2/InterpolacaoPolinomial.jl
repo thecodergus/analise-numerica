8603f1bd35fe096fb203e085c4bd4184a0fd1944
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-12 17:35:39 -0300

    Ajuste

--- T2/InterpolacaoPolinomial.jl
+++ T2/InterpolacaoPolinomial.jl
@@ -4,9 +4,9 @@ module InterpolacaoPolinomial
     end
 
     function build_polynomial_function(coords_x::Vector{<:Real}, coords_y::Vector{<:Real}, n::Int64=length(coords_x))
-        coefs::Vector{<:Real} = vanderMonde(coords_x, coords_y, n)
-
-        return build_polynomial_function!(coefs)
+        return build_polynomial_function!(
+                vanderMonde(coords_x, coords_y, n)
+            )
     end
 
     function build_polynomial_function!(coefs::Vector{<:Real})
