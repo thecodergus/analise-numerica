00e3dd478ad18dd4fc890bcce4dba92c8aac04e9
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-16 21:55:14 -0300

    Mais alguns exercicios feitos e um pdf top pra ajudar

--- T2/InterpolaçãoPolinomial.jl
+++ T2/InterpolaçãoPolinomial.jl
@@ -1,12 +1,14 @@
 
 module InterpolaçãoPolinomial
+    include("tool.jl")
     vector_of_vectors_to_matrix(a) = reduce(vcat,transpose.(a))
 
     function vanderMonde(coords_x::Vector{<:Real}, coords_y::Vector{<:Real}, grau::Int64):: Vector{<:Real}
         return vector_of_vectors_to_matrix(map(x -> [x^i for i = 0:grau], coords_x)) \ coords_y
     end
 
-    function regressão_linear(coords_x::Vector{<:Real}, coords_y::Vector{<:Real}, grau::Int64)
+    # Ajusto de curva comum - Regressão Linear
+    function AjusteDeCurva(coords_x::Vector{<:Real}, coords_y::Vector{<:Real}, grau::Int64)
         # esquerdo da equação
         X = vector_of_vectors_to_matrix([[sum(map(x -> x^(i + j), coords_x)) for j in 0:grau] for i in 0:grau])
         # direito da equação
@@ -15,6 +17,64 @@ module InterpolaçãoPolinomial
         coefs = X \ Y
 
         return (X, coefs, Y)
+    end
+
+    # Ajuste a uma curva exponencial - Regressão Linear
+    # y = a*(e^b*x) <=> ln y = ln a + b*x 
+    # adaptação: y = a*(t^b*x) onde t e seu log log correspondete variam
+    # Questão chata pra caralho, questão: Q12 de Ajuste de Curvas
+    function AjusteDeCurvaExponencial(coords_x::Vector{<:Real}, coords_y::Vector{<:Real}, grau::Int64 = 1; t = ℯ, ant_t = log)
+        # pq Y = ln y
+        map!(ant_t, coords_y, coords_y)
+        
+        # esquerdo da equação
+        X = vector_of_vectors_to_matrix([[sum(map(x -> x^(i + j), coords_x)) for j in 0:grau] for i in 0:grau])
+        # direito da equação
+        Y = vector_of_vectors_to_matrix([sum([yi * xi^i for (xi, yi) = zip(coords_x, coords_y)]) for i = 0:grau]) 
+        # coeficiente gerados
+        coefs = X \ Y
+
+        coefs_cp = copy(coefs)
+        coefs[1] = t^coefs[1]
+        
+        # Retornar os coeficientes, e a função adaptada para a situação
+        return (coefs, x::Real -> t^(sum([valor * x^index for (index, valor) in zip(Iterators.countfrom(0), coefs_cp)])))
+    end
+
+    # Ajuste a uma curva geométrica - Regressão Linear
+    # Para funções do tipo y = a*x^b <=> ln y = ln a + x*ln b = a + b*x
+    function AjusteDeCurvaGeométrica(coords_x::Vector{<:Real}, coords_y::Vector{<:Real}, grau::Int64 = 1; t = ℯ, ant_t = log)
+        map!(ant_t, coords_y, coords_y)
+        map!(ant_t, coords_x, coords_x)
+
+        # esquerdo da equação
+        X = vector_of_vectors_to_matrix([[sum(map(x -> x^(i + j), coords_x)) for j in 0:grau] for i in 0:grau])
+        # direito da equação
+        Y = vector_of_vectors_to_matrix([sum([yi * xi^i for (xi, yi) = zip(coords_x, coords_y)]) for i = 0:grau]) 
+        # coeficiente gerados
+        coefs = X \ Y
+
+
+        coefs_cp = copy(coefs)
+        coefs[1] = t^coefs[1]
+
+        # Coeficientes e a função adaptada
+        return (coefs, x::Real -> t^(sum([valor * ant_t(x)^index for (index, valor) in zip(Iterators.countfrom(0), coefs_cp)])))
+    end
+
+    # Ajusto de curva hiperbolica - Regressão Linear
+    # z = 1/y = a + b*x
+    function AjusteDeCurvaHiperbolica(coords_x::Vector{<:Real}, coords_y::Vector{<:Real}, grau::Int64)
+        map!(y -> 1 / y, coords_y, coords_y)
+        
+        # esquerdo da equação
+        X = vector_of_vectors_to_matrix([[sum(map(x -> x^(i + j), coords_x)) for j in 0:grau] for i in 0:grau])
+        # direito da equação
+        Y = vector_of_vectors_to_matrix([sum([yi * xi^i for (xi, yi) = zip(coords_x, coords_y)]) for i = 0:grau]) 
+        # coeficiente gerados
+        coefs = X \ Y
+
+        return coefs
     end
 
     function build_polynomial_function(coefs::Vector{<:Real})::Function
--- T2/main.jl
+++ T2/main.jl
@@ -3,20 +3,25 @@ include("tool.jl")
 
 
 
-x = [0.0881, 0.2199, 0.3709, 0.552, 0.713, 0.8487, 1.1358, 1.2648, 1.4314, 1.6439, 1.7936, 1.8714]
-y = [6.9983, 7.6152, 8.1006, 11.0015, 13.9742, 16.5418, 27.3878, 32.7363, 41.555, 59.1029, 73.3571, 83.2471]
-values = [1.3774, 1.5064, 1.5594]
 
 
 
-(X, coeffs, Y) = InterpolaçãoPolinomial.regressão_linear(x, y, 1)
+x = [1.454, 2.71, 5.4475, 5.9073, 7.9261, 9.9695, 11.9958, 12.1671, 14.6046, 16.1558, 18.2149, 19.5855]
+y = [2.3399, 2.9291, 3.3685, 3.4383, 3.5481, 3.6839, 3.7044, 3.6355, 3.6948, 3.7906, 3.8051, 3.8312]
+values = [5.5459, 15.9459, 17.6236]
 
+
+
+# (coeffs, f) = InterpolaçãoPolinomial.AjusteDeCurvaGeométrica(x, y, 1)
+coeffs = InterpolaçãoPolinomial.AjusteDeCurvaHiperbolica(x, y, 1)
 f = InterpolaçãoPolinomial.build_polynomial_function(coeffs)
+
 
 # printMatrix(X)
 printVector(coeffs)
 # printVector(Y)
+# print(f)
 
-for i = values
-    printItem(f(i))
-end
+# for i = values
+#     printItem(f(i))
+# end
