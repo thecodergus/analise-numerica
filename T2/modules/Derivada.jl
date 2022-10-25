80d22f359484e794d60568ccecd0a3bb1a7c3819
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-25 17:46:05 -0300

    simplificações

--- T2/modules/Derivada.jl
+++ T2/modules/Derivada.jl
@@ -1,24 +1,17 @@
 module Derivada
     function derivada_finita(f::Function, x0, coords_x; ordem::Integer = 1)
-        n::Integer = length(coords_x)
-        coords_y = f.(coords_x)
-
-        A::Matrix{Real}, B::Vector{Real} = ones(1, n), [0.0]
+        n::Integer, coords_y = length(coords_x), f.(coords_x)
+        A::Matrix{Real}, B::Vector{Real} = ones(1, n), zeros(1)
 
         for i = 1:(n - 1)
-            row_i = coords_x .^ i
-            A = vcat(A, row_i')
+            A = vcat(A, (coords_x .^ i)')
 
             if i < ordem
                 push!(B, 0)
             elseif i == ordem
                 push!(B, factorial(ordem))
             else
-                numer, denom = factorial(i), factorial(i - ordem)
-
-                el = (numer / denom) * x0^(i - ordem)
-
-                push!(B, el)
+                push!(B, (factorial(i) / factorial(i - ordem)) * x0^(i - ordem))
             end
         end
 
