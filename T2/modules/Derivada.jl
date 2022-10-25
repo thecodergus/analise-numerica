ba6d2e6544e1e2f69bf44c8d923d495afc3e81d1
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-25 15:03:18 -0300

    Tentativa 1

--- T2/modules/Derivada.jl
+++ T2/modules/Derivada.jl
@@ -1,3 +1,29 @@
 module Derivada
-    
+    function derivada_finita(f::Function, x0, coords_x; ordem::Integer = 1, n::Integer = length(coords_x))
+        A::Matrix{Real}, B::Vector{Real} = ones(1, n), [0.0]
+        coords_y = f.(coords_x)
+
+        for i = 1:(n - 1)
+            row_i = coords_x .* i
+            A = vcat(A, row_i')
+
+            if i < ordem
+                push!(B, 0)
+            elseif i == ordem
+                push!(B, factorial(ordem))
+            else
+                numer, denom = factorial(i), factorial(i - ordem)
+
+                el = (numer / denom) * x0^(i - ordem)
+
+                push!(B, el)
+            end
+        end
+        println(A)
+        println(B)
+
+        # coeffs = A \ B
+
+        # return sum(coeffs .* coords_y)
+    end
 end
