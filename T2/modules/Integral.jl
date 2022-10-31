10a75f377d18f9d605b61b12520b4b40df1c8782
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-31 18:21:58 -0300

    Mais uns algoritmos

--- T2/modules/Integral.jl
+++ T2/modules/Integral.jl
@@ -11,7 +11,13 @@ module Integral
 
     function Regra_de_Simpson(f::Function, variavel_inferior::Float64, variavel_superior::Float64, intervalos::Int64)::Float64
         h::Float64 = (variavel_superior - variavel_inferior) / intervalos
-        
+
         return (h/3) * (f(variavel_inferior) + 4 * sum(f.((collect(1:2:(intervalos - 1)) .* h) .+ variavel_inferior)) + 2 * sum(f.((collect(2:2:(intervalos - 1)) .* h) .+ variavel_inferior)) + f(variavel_superior))
+    end
+
+    function Regra_de_Simpson_Ponto(x::Vector{Float64}, y::Vector{Float64})::Float64
+        regra_de_simpson(x0, x1, y0, y1, y2) = ((x1-x0)/3)*(y0+4*y1+y2)
+        
+        return sum([regra_de_simpson(x[k],x[k+1],y[k],y[k+1],y[k+2]) for k = 1:2:(length(x) - 1)])
     end
 end
--- T2/t.py
+++ T2/t.py
@@ -1,20 +1,40 @@
-# import math
-from numpy import *
-# Usado para aproximar o valor de uma integral
-def trapz(f, a, b, n):
-    h = (b-a) / n
-    soma = 0
-    for k in range(1, n):
-        soma += f(a + k*h)
-    soma *= 2
-    soma += (f(a) + f(b))
-    return (h/2) * soma
+import math
+
+def simps(f, a, b, n):
+    if n % 2 != 0 or n < 1:
+        raise ValueError("n deve ser par e maior q 1")
+    h = (b - a) / n
+    soma_odd, soma_even = 0, 0
+    for k in range(1,n,2):
+        soma_odd += f(a+k*h)
+    for k in range(2,n,2):
+        soma_even += f(a + k * h)
+    return (h/3) * (f(a) + 4 * soma_odd + 2 * soma_even + f(b))
+
+def f(x):
+    return math.sqrt(1 + math.cos(x)**2)
+
+def simp(x0,x1,x3,y0,y1,y2):
+    return ((x1-x0)/3)*(y0+4*y1+y2)
+
+def simpsPonto(x, y):
+    tam = (len(x) - 1) // 2
+    somas = 0
+    k = 0
+    for i in range(tam):
+        somas += simp(x[k],x[k+1],x[k+2],y[k],y[k+1],y[k+2])
+        k += 2
+    print(f'{somas}')
 
 
-f = lambda x: sin(e**(-x**2)) + 1
-a, b = [-1.68, 1.752]
-subintervalos = [1, 11, 31, 74, 76, 147, 172, 381, 605, 790, 2863, 6030]
+x = [1.689, 2.382, 3.075, 3.4025, 3.73]
+y = [2.097, 2.145, 2.915, 2.923, 2.148]
 
-for i in range(len(subintervalos)):
-    r = trapz(f, a, b, subintervalos[i])
-    print(r)
+
+'''
+n = len(subintervalos)
+for i in range(n):
+    print(simps(f, intervalo[0], intervalo[1], subintervalos[i]))
+'''
+
+simpsPonto(x, y)
