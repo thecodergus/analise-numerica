994e9e1a7523ce575b3310a1fdee38f02fd456ce
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-11-25 18:31:17 -0300

    ajustes de nomenclaturas

---
+++ T3/main.jl
@@ -0,0 +1,5 @@
+include("tools/funções.jl")
+include("modules/Otimização.jl")
+
+
+println(criar_função("x + 2")(3))
--- T3/tools/funções.jl
+++ T3/tools/funções.jl
@@ -1,27 +1,23 @@
+using PythonCall
+math = pyimport("math")
+export criar_função
 
-module funções
-    using PythonCall
-    math = pyimport("math")
-
-    macro expr2fn(fname, expr, args...)
-        fn = quote
-            function $(esc(fname))()
-                $(esc(expr.args[1]))
-            end
+macro expr2fn(fname, expr, args...)
+    fn = quote
+        function $(esc(fname))()
+            $(esc(expr.args[1]))
         end
-        for arg in args
-            push!(fn.args[2].args[1].args, esc(arg))
-        end
-        return fn
     end
-
-    function criar_função(f::String)::Function
-        return @eval x -> $(f |> Meta.parse)
+    for arg in args
+        push!(fn.args[2].args[1].args, esc(arg))
     end
+    return fn
+end
 
-    function map_function(arr::Vector{String})::Vector{Function}
-        return criar_função.(arr)    
-    end
+function criar_função(f::String)::Function
+    return @eval x -> $(f |> Meta.parse)
+end
 
-    export criar_função
+function map_function(arr::Vector{String})::Vector{Function}
+    return criar_função.(arr)    
 end
