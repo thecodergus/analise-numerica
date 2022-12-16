44900d46ad621c33d37f93eb8071634d062a4ce4
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-12-16 11:59:11 -0300

    Edição que fiz pra prova

--- T3/novak/17.py
+++ T3/novak/17.py
@@ -1,5 +1,8 @@
 import math
+from re import A
 import numpy as np
+from cachetools import cached, LRUCache, TTLCache
+
 
 
 def trapz(f, a, b, n):
@@ -11,17 +14,36 @@ def trapz(f, a, b, n):
 
     return (f(a) + 2 * sum_fx + f(b)) * h / 2
 
+def simps(f, a, b, n):
+    if n % 2 != 0:
+        print('O valor n deve ser par')
+        return None
+
+    num_parabolas = n / 2
+    soma = 0
+    h = (b - a) / n
+
+    for i in range(int(num_parabolas)):
+        x0 = a + (2 * i) * h
+        x1 = a + (2 * i + 1) * h
+        x2 = a + (2 * i + 2) * h
+        soma += f(x0) + 4 * f(x1) + f(x2)
+
+    soma *= h / 3
+
+    return soma
+
 
 def coef(f,g):
     # a = -1.05973 
     # b=1.4086
-    a=-1.15018
-    b=1.30837
-    n = 256
+    a=-1.07173
+    b=1.45137
+    n = 500 
     func = lambda x: (f(x) * g(x) ) 
     func2 = lambda x: g(x) * g(x)
-    numer = trapz(func,a,b,n)
-    denom = trapz(func2,a,b,n)
+    numer = simps(func,a,b,n)
+    denom = simps(func2,a,b,n)
     
     return (numer/denom)
 
@@ -50,41 +72,43 @@ def coef(f,g):
 #         list_coefs.append(ck)
 #     return list_coefs
         
-if __name__ == '__main__':
-    
-    def f1(x): return 1
-    def f2(x): return x
-    def f3(x): return x**2
-    def f4(x): return x**3
-    
-    def g1(x): 
-        return f1(x)
+# fs = [lambda x: x**i for i in range(10)]
+fs = [
+    lambda x: x**0,
+    lambda x: x**1,
+    lambda x: x**2,
+    lambda x: x**3,
+    lambda x: x**4,
+    lambda x: x**5,
+    lambda x: x**6,
+    lambda x: x**7,
+    lambda x: x**8,
+    lambda x: x**9,
+]
 
-    a_21 =  coef(f2, g1)
-    print(f'{a_21},')
-    
-    def g2(x):
-        return f2(x) - a_21*g1(x)
-    
-    a_31 = coef(f3, g1)
-    a_32 = coef(f3, g2)
-    
-    print(f'{a_31},')
-    print(f'{a_32},')
-    
-    def g3(x): 
-        return f3(x) - a_31*g1(x) - a_32*g2(x)
-    
-    a_41 = coef(f4, g1)
-    a_42 = coef(f4, g2)
-    a_43 = coef(f4, g3)
-    
-    print(f'{a_41},')
-    print(f'{a_42},')
-    print(f'{a_43},')
-   
-    def g4(x): 
-        return f4(x) - a_41*g1(x) - a_42*g2(x) - a_43*g3(x)
+def f(num):
+    # print(f"f - {num}")
+    if num == 1:
+        return lambda x: 1
+    return fs[num - 1]
+
+def g(num):
+    if num == 1:
+        return lambda x: f(num)(x)
+
+    # print(f"g - {num}")
+    return lambda x: f(num)(x) - sum([a(num, i) * g(i)(x) for i in range(1, num)])
+
+@cached(cache={})
+def a(a, b):
+    return coef(f(a), g(b))
+
+if __name__ == '__main__':
+    question = [[(a, b) for b in range(1, a)] for a in range(2, 11)]
+
+    for linha in question:
+        for a1, b1 in linha:
+            print(f"{a(a1, b1)},", end="")
     
     
         
